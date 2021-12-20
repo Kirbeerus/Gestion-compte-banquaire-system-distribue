6d8926909b9df076934818f42df692517ca48210
coco <corentin.barault@gmail.com>
2021-12-20 15:07:09 +0100

    jeu de test

---
+++ src/main/java/sd/AppSingleClient.java
@@ -0,0 +1,206 @@
+package sd;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.pattern.Patterns;
+import akka.routing.RoundRobinPool;
+import sd.Actor.*;
+
+public class AppSingleClient {
+	public static void main(String[] args) throws InterruptedException{
+		ActorSystem actorSystem = ActorSystem.create();
+
+        ActorRef banque = actorSystem.actorOf(BanqueActor.props());
+
+        ActorRef client = actorSystem.actorOf(ClientActor.props(banque,1));
+        client.tell(new ClientActor.Connexion(0), ActorRef.noSender());
+
+
+        //Initialisation du programme---------------------------------------------------
+        for(int i = 0;i<10000;i++) {
+        	client.tell(new ClientActor.StartAjout(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        
+        long startTime = System.currentTimeMillis();
+        
+        System.out.println("10 Demande de retrait");
+        //10 demande de retrait-----------------------------------------------------
+        for(int i = 0;i<10;i++) {
+        	client.tell(new ClientActor.StartRetirer(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        
+		Thread.sleep(100);
+		
+        startTime = System.currentTimeMillis();
+
+        System.out.println("10 Demande d'ajout");
+        //10 demande d'ajout---------------------------------------------------
+        for(int i = 0;i<10;i++) {
+        	client.tell(new ClientActor.StartAjout(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+        startTime = System.currentTimeMillis();
+
+        System.out.println("100 Demande de retrait");
+        //100 demande de retrait-----------------------------------------------------
+        for(int i = 0;i<100;i++) {
+        	client.tell(new ClientActor.StartRetirer(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+        startTime = System.currentTimeMillis();
+
+        System.out.println("100 Demande d'ajout");
+        //100 demande d'ajout---------------------------------------------------
+        for(int i = 0;i<100;i++) {
+        	client.tell(new ClientActor.StartAjout(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+        startTime = System.currentTimeMillis();
+
+        System.out.println("1000 Demande de retrait");
+        //1 000 demande de retrait-----------------------------------------------------
+        for(int i = 0;i<1000;i++) {
+        	client.tell(new ClientActor.StartRetirer(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+        startTime = System.currentTimeMillis();
+
+        System.out.println("1000 Demande d'ajout");
+        //1 000 demande d'ajout---------------------------------------------------
+        for(int i = 0;i<1000;i++) {
+        	client.tell(new ClientActor.StartAjout(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+        startTime = System.currentTimeMillis();
+        System.out.println("10 000 Demande de retrait");
+      //10 000 demande de retrait----------------------------------------------------------------------
+        
+
+        for(int i = 0;i<10000;i++) {
+        	client.tell(new ClientActor.StartRetirer(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+       startTime = System.currentTimeMillis();
+
+       System.out.println("10 000 Demande d'ajout");
+      //10 000 demande d'ajout--------------------------------------------------------------------
+        for(int i = 0;i<10000;i++) {
+        	client.tell(new ClientActor.StartAjout(), ActorRef.noSender());
+        }
+
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+        client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+        Thread.sleep(100);
+        startTime = System.currentTimeMillis();
+        System.out.println("100 000 Demande de retrait");
+        //100 000 demande de retrait----------------------------------------------------------------------
+          
+
+          for(int i = 0;i<100000;i++) {
+          	client.tell(new ClientActor.StartRetirer(), ActorRef.noSender());
+          }
+
+          try {
+  			Thread.sleep(5000);
+  		} catch (InterruptedException e) {
+  			e.printStackTrace();
+  		}
+
+          client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+          Thread.sleep(100);
+         startTime = System.currentTimeMillis();
+
+         System.out.println("100 000 Demande d'ajout");
+        //100 000 demande d'ajout--------------------------------------------------------------------
+          for(int i = 0;i<100000;i++) {
+          	client.tell(new ClientActor.StartAjout(), ActorRef.noSender());
+          }
+
+          try {
+  			Thread.sleep(5000);
+  		} catch (InterruptedException e) {
+  			e.printStackTrace();
+  		}
+
+          client.tell(new ClientActor.AfficherSolde(startTime), ActorRef.noSender());
+
+        // Arrêt du système d'acteurs
+        actorSystem.terminate();
+	}
+} 
